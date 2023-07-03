77f40379a0fde0f0f54c95b3ef3c7281bb771b10
Devohka <110058157+Devohka@users.noreply.github.com>
2023-07-03 22:49:32 +0300

    js

---
+++ js/3-07.js
@@ -0,0 +1,104 @@
+ // 1
+// const myArray = {
+//     age: 34,
+// };
+
+// console.log(myArray);// до додавання
+// console.log(myArray.name = "Oleg-bobr");// додавання
+// console.log(myArray.age = 45);// міняєм
+// console.log(myArray.city = "Brazil");// додавання
+// console.log(myArray.occupation);// undefined томущо такого немає у об`єкті
+// console.log(myArray);//після додавання
+
+// 2
+// const isAvailable = "in stock";
+// const yesAvailablee = "yes";
+// const noAvailablee = "no";
+// const cardCucumber = {
+//   varieties: [
+//     {
+//       name: "Rodnichok cucumber",
+//       prsce: 60,
+//       [isAvailable]: yesAvailablee,
+//     },
+//     {
+//       name: "Cucumber Spring",
+//       prsce: 45,
+//       [isAvailable]: yesAvailablee,
+//     },
+//     {
+//       name: "Parazytsky cucumber",
+//       prsce: 35,
+//       [isAvailable]: noAvailablee,
+//     },
+//   ],
+// };
+// console.log(cardCucumber);
+
+// 3
+
+// const apartment = {
+//   imgUrl: "https://via.placeholder.com/640x480",
+//   descr: "Spacious apartment in the city center",
+//   rating: 4.7,
+//   price: 5000,
+//   tags: ["premium", "promoted", "top", "trusted"],
+//   owner: {
+//     name: "Henry Sibola",
+//     phone: "982-126-1588",
+//     email: "henry.carter@aptmail.com",
+//   },
+//   location: {
+//       country: "Jamaica",
+//       city: "Kingston",
+//   },
+//   aptRating() {
+//     return this.rating;
+//   },
+//   aptDescr() {
+//     return this.descr;
+//   },
+//   aptPrice() {
+//     return this.price;
+//   },
+//   aptTags() {
+//     return this.tags;
+//   },
+// };
+// apartment.area = 60;
+// apartment.rooms = 3;
+// console.log(apartment);
+
+// 4
+
+// const name = "Repair Droid";
+// const price = 2500;
+// const image = "https://via.placeholder.com/640x480";
+// const tags = ["on sale", "trending", "best buy"];
+
+// const product = {
+//   // Change code below this line
+//     name,
+//     price,
+//     image,
+//     tags,
+//   // Change code above this line
+// };
+// console.log(product);
+
+// 5
+
+
+// const emailInputName = "email";
+// const passwordInputName = "password";
+// const email = "henry.carter@aptmail.com";
+// const password = "jqueryismyjam";
+
+// const credentials = {
+//   // Change code below this line
+//   [emailInputName]: email,
+//   [passwordInputName]: password,
+
+//   // Change code above this line
+// };
+// console.log(credentials);
